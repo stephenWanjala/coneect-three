dc26bcc8660d1e2df1aa03e5b51fef19098ca5f0
wanjala stephen <stephenwanjala145@gmail.com>
2022-06-10 00:34:48 +0300

    manage game Is active'

--- app/src/main/java/com/wtech/connect3/MainActivity.kt
+++ app/src/main/java/com/wtech/connect3/MainActivity.kt
@@ -10,6 +10,7 @@ import kotlin.properties.Delegates
 
 
 class MainActivity : AppCompatActivity() {
+    private var gameIsActive by Delegates.notNull<Boolean>()
     private lateinit var progressDiaLog: AlertDialog
     private lateinit var gameState: Array<Int>
     private lateinit var winingPositions: Array<Array<Int>>
@@ -21,6 +22,7 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         activePlayer = 0
+        gameIsActive=true
         gameState = arrayOf(2, 2, 2, 2, 2, 2, 2, 2, 2)
 
 
@@ -64,7 +66,7 @@ class MainActivity : AppCompatActivity() {
 
         imageClicked.alpha = 1f
         val imageTapped: Int = imageClicked.tag.toString().toInt()
-        if (gameState[imageTapped] == 2) {
+        if (gameState[imageTapped] == 2 && gameIsActive) {
             gameState[imageTapped] = activePlayer
             imageClicked.translationY = -1000f
             activePlayer = if (activePlayer == 0) {
@@ -86,55 +88,56 @@ class MainActivity : AppCompatActivity() {
                     gameState[winingPosition[0]] == gameState[winingPosition[1]]
                             && gameState[winingPosition[1]] == gameState[winingPosition[2]]
                             && gameState[winingPosition[0]] != 2 -> {
+                        gameIsActive=false
                         var winner = "Green"
                         if (gameState[winingPosition[0]] == 0) {
                             winner = "Yellow"
                         }
-//                        binding.winnerText.text = "Winner: $winner"
-//                        binding.linear.animate().y(-1000f)
+                        binding.winnerText.text = "Winner: $winner"
+                        binding.linear.animate().y(-1000f)
+
+                        binding.linear.animate().alphaBy(1f).duration=300
+                        binding.linear.visibility = View.VISIBLE
+                        binding.linear.animate()
+                            .rotation(360f)
+                            .translationYBy(100f)
+                            .duration=300
+
+
+////                        val alertDialog = AlertDialog.Builder(this) //set icon
+//                            .setIcon(android.R.drawable.ic_dialog_info) //set title
+//                            .setTitle("PLAy Again") //set message
+//                            .setMessage("The winner is: $winner")
+//                            //set positive button
 //
-//                        binding.linear.animate().alphaBy(1f).duration=300
-//                        binding.linear.visibility = View.VISIBLE
-//                        binding.linear.animate()
-//                            .rotation(360f)
-//                            .translationYBy(100f)
-//                            .duration=300
-
-
-                        val alertDialog = AlertDialog.Builder(this) //set icon
-                            .setIcon(android.R.drawable.ic_dialog_info) //set title
-                            .setTitle("PLAy Again") //set message
-                            .setMessage("The winner is: $winner")
-                            //set positive button
-
-                            .setPositiveButton("Yes") { dialogInterface, i ->
-                                //set what would happen when positive button is clicked
-                                activePlayer = 0
-                                for (i in 0..gameState.lastIndex) {
-                                    gameState[i] = 2
-                                }
-
-                                binding.apply {
-                                    img0.setImageResource(0)
-                                    img8.setImageResource(0)
-                                    img1.setImageResource(0)
-                                    img2.setImageResource(0)
-                                    img3.setImageResource(0)
-                                    img4.setImageResource(0)
-                                    img5.setImageResource(0)
-                                    img6.setImageResource(0)
-                                    img7.setImageResource(0)
-                                }
-
-                                dialogInterface.dismiss()
-
-                            } //set negative button
-                            .setNegativeButton("Exit") { dialogInterface, i ->
-                                dialogInterface.dismiss()//set what should happen when negative button is clicked
-
-                                finish()
-                            }
-                            .show()
+//                            .setPositiveButton("Yes") { dialogInterface, i ->
+//                                //set what would happen when positive button is clicked
+//                                activePlayer = 0
+//                                for (i in 0..gameState.lastIndex) {
+//                                    gameState[i] = 2
+//                                }
+//
+//                                binding.apply {
+//                                    img0.setImageResource(0)
+//                                    img8.setImageResource(0)
+//                                    img1.setImageResource(0)
+//                                    img2.setImageResource(0)
+//                                    img3.setImageResource(0)
+//                                    img4.setImageResource(0)
+//                                    img5.setImageResource(0)
+//                                    img6.setImageResource(0)
+//                                    img7.setImageResource(0)
+//                                }
+//
+//                                dialogInterface.dismiss()
+//
+//                            } //set negative button
+//                            .setNegativeButton("Exit") { dialogInterface, i ->
+//                                dialogInterface.dismiss()//set what should happen when negative button is clicked
+//
+//                                finish()
+//                            }
+//                            .show()
 
 
                     }
